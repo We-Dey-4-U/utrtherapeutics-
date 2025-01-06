08c913ffeaf3ba31a02b221ab9e1e57425ec053a
ikenna <mrteks2022@gmail.com>
2025-01-06 01:58:59 -0800

    ADDRESS

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract InnovX is ERC20 {
-    constructor() ERC20("InnovX", "$INX") {
+contract UTR is ERC20 {
+    constructor() ERC20("UTR", "$UTR") {
         _mint(msg.sender, 1000000000000000000000000000); // 1 billion tokens with 18 decimals
     }
 }
