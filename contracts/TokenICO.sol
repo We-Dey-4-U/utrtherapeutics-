e8dc1837ec5ce946440b5432e38d0cbabcf577b9
ikenna <mrteks2022@gmail.com>
2025-03-28 07:26:47 -0700

    push

--- contracts/TokenICO.sol
+++ contracts/TokenICO.sol
@@ -94,3 +94,7 @@ contract TokenICO {
 
 
 //main contract
+/// title A title that should describe the contract/interface
+/// author The name of the author
+/// notice Explain to an end user what this does
+/// dev Explain to a developer any extra details
